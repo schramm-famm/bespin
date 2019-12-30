718fa2fc2544bb87de83ce1849d7db142a179c2c
Igor Veselinovic <igor@veselinovic.net>
2019-12-30 18:44:39 -0500

    Add ECS cluster and instance profile config
    Just creates an ECS cluster and IAM instance role and policy
    attachments.
    
    Also added a "name" variable to differentiate deployments.

--- main.tf
+++ main.tf
@@ -4,8 +4,41 @@ provider "aws" {
   region     = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-00bf61217e296b409"
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+resource "aws_ecs_cluster" "riht" {
+  name = var.name
+}
+
+resource "aws_iam_role" "ecs_agent" {
+  name = "${var.name}_ecs_instance_role"
+  path = "/ecs/"
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "ecs_agent" {
+  name = "${var.name}_ecs_instance_profile"
+  role = aws_iam_role.ecs_agent.name
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_agent_role" {
+  role = aws_iam_role.ecs_agent.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_agent_cloudwatch_role" {
+  role = aws_iam_role.ecs_agent.id
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
