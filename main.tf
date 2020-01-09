dde685a988d5796580eade1b5e58a9ea2227ce6c
Igor Veselinovic <igor@veselinovic.net>
2020-01-09 18:22:31 -0500

    Rewrite TF config to canonical format
    Used `terraform fmt` command

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ provider "aws" {
 }
 
 module "ecs_base" {
-  source = "./modules/ecs_base"
-  name = var.name
+  source             = "./modules/ecs_base"
+  name               = var.name
   enable_nat_gateway = false
 }
 
 module "ecs_cluster" {
-  source = "./modules/ecs_cluster"
-  name = var.name
-  security_group_id = module.ecs_base.vpc_default_security_group_id
-  subnets = module.ecs_base.vpc_private_subnets
+  source                  = "./modules/ecs_cluster"
+  name                    = var.name
+  security_group_id       = module.ecs_base.vpc_default_security_group_id
+  subnets                 = module.ecs_base.vpc_private_subnets
   ec2_instance_profile_id = module.ecs_base.ecs_instance_profile_id
 }
--- modules/ecs_base/main.tf
+++ modules/ecs_base/main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "ecs_agent" {
-  name = "${var.name}_ecs_instance_role"
-  path = "/ecs/"
+  name               = "${var.name}_ecs_instance_role"
+  path               = "/ecs/"
   assume_role_policy = <<EOF
 {
   "Version": "2008-10-17",
@@ -24,12 +24,12 @@ resource "aws_iam_instance_profile" "ecs_agent" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_agent_role" {
-  role = aws_iam_role.ecs_agent.id
+  role       = aws_iam_role.ecs_agent.id
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_agent_cloudwatch_role" {
-  role = aws_iam_role.ecs_agent.id
+  role       = aws_iam_role.ecs_agent.id
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
