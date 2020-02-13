32f5de4623068559349c94bc123d17995fc4c1ef
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2020-02-13 11:47:27 -0500

    Add vpc_id output

--- modules/ecs_base/output.tf
+++ modules/ecs_base/output.tf
@@ -10,6 +10,10 @@ output "vpc_public_subnets" {
   value = module.vpc.public_subnets
 }
 
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
 output "ecs_instance_profile_id" {
   value = aws_iam_instance_profile.ecs_agent.id
 }
