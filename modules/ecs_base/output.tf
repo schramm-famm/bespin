bad07bcf732d6e9cc28d9d2dfa60062dc27f38ee
Igor Veselinovic <igor@veselinovic.net>
2020-03-27 20:36:01 -0400

    Add CIDR block output for ECS base

--- modules/ecs_base/output.tf
+++ modules/ecs_base/output.tf
@@ -10,6 +10,10 @@ output "vpc_public_subnets" {
   value = module.vpc.public_subnets
 }
 
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
+
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
